7c514ff3363f263c4405d1cc1618439626c6e23a
Matheus Pazetti <matheus@aalce.com.br>
2022-01-12 15:58:43 -0300

    section tarefa

---
+++ assets/tarefa1.js
@@ -0,0 +1,19 @@
+class ShopifyAcademyTarefa1 extends HTMLElement {
+  constructor() {
+    super();
+    
+    this.addAllProducts = this.querySelector('#addAllProducts');
+
+    this.init();
+
+  }
+
+  init() {
+    this.addAllProducts.addEventListener('click', this.addProducts);
+  }
+
+  addProducts() {
+    console.log('>> addProducts')
+  }
+}
+customElements.define('shopify-academy-tarefa1', ShopifyAcademyTarefa1);
